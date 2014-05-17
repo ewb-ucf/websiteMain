e70d2cf67966408c1c479dad4464aeb3f94928aa
mckinnley <mck.workman@gmail.com>
2014-05-17 16:37:06 -0400

    cinstruction pages

--- main/app/models/member.rb
+++ main/app/models/member.rb
@@ -6,7 +6,7 @@ class Member < ActiveRecord::Base
 	# 	username
 	# end	
 
-	has_and_belongs_to_many :roles
+	# has_and_belongs_to_many :teams
 	has_many :projects 
 
 	attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :gender, :pid, :aboutme, :skills, :experience, :phone
--- main/config/routes.rb
+++ main/config/routes.rb
@@ -1,89 +1,122 @@
 Rails.application.routes.draw do
 
+# Custom Routes: 
+# As the pages are created simply comment out the construction route
+# and uncomment the route to the actual page
+
 # Home Page
 match '/', to:'static_pages#home', via:'get' 
   
 # About Us Tab
 match '/about', to:'static_pages#about', via:'get' 
+# match '/about', to:'static_pages#under_construction', via:'get' 
 
 # Our People --> Our Partners
 match '/partners', to:'static_pages#partners', via:'get' 
+# match '/partners', to:'static_pages#under_construction', via:'get' 
 
 # Our People 
-match '/members', to:'members#show',  via:'get'
+# match '/members', to:'members#show',  via:'get'
+match '/members', to:'static_pages#under_construction', via:'get' 
   resources :members
 
   # Exec Board
   match '/exec_board', to:'members#exec_board',  via:'get'
+  # match '/exec_board', to:'static_pages#under_construction', via:'get' 
 
   # International Project Team
   match '/intl_project_team', to:'members#intl_project_team',  via:'get'
+  # match '/intl_project_team', to:'static_pages#under_construction', via:'get' 
 
   # Local Project Team
   match '/local_project_team', to:'members#local_project_team',  via:'get'
+  # match '/local_project_team', to:'static_pages#under_construction', via:'get' 
 
   # Software Team
     match '/software_team', to:'members#software_team',  via:'get'
+    # match '/software_team', to:'static_pages#under_construction', via:'get' 
 
   # Alum
   match '/alum', to:'members#alum',  via:'get'
+  # match '/alum', to:'static_pages#under_construction', via:'get' 
 
 # Our Projects
-match '/projects',  to:'projects#show',  via:'get'
+# match '/projects',  to:'projects#show',  via:'get'
+match '/projects', to:'static_pages#under_construction', via:'get' 
 
     # Local Projects (bithlo, rainwater catchment)
-    match '/projects/local',  to:'projects#local',  via:'get'
+    # match '/projects/local',  to:'projects#local',  via:'get'
+    match '/projects/local', to:'static_pages#under_construction', via:'get' 
 
     # International Projects (weather station, dr)
-    match '/projects/international',  to:'projects#international',  via:'get'
+    # match '/projects/international',  to:'projects#international',  via:'get'
+    match '/projects/international', to:'static_pages#under_construction', via:'get' 
 
     # Software Projects
-    match '/projects/software',  to:'projects#software',  via:'get'
+    # match '/projects/software',  to:'projects#software',  via:'get'
+    match '/projects/software', to:'static_pages#under_construction', via:'get' 
 
     # Other Projects
-    match '/projects/other',  to:'projects#other',  via:'get'
+    # match '/projects/other',  to:'projects#other',  via:'get'
+    match '/projects/other', to:'static_pages#under_construction', via:'get' 
 
 # Events
-match '/events',  to:'events#show',  via:'get'
+# match '/events',  to:'events#show',  via:'get'
+match '/events', to:'static_pages#under_construction', via:'get' 
 
   # Workshops
-  match '/workshops',  to:'events#workshops',  via:'get'
+  # match '/workshops',  to:'events#workshops',  via:'get'
+  match '/workshops', to:'static_pages#under_construction', via:'get' 
 
   # Lectures
-  match '/lectures',  to:'events#lectures',  via:'get'
+  # match '/lectures',  to:'events#lectures',  via:'get'
+  match '/lectures', to:'static_pages#under_construction', via:'get'
 
 # Media (photos and videos)
-match '/media',  to:'medias#show',  via:'get' 
+# match '/media',  to:'medias#show',  via:'get' 
+match '/media', to:'static_pages#under_construction', via:'get' 
 
   # Photos (categorized by event+date)
-  match '/media/photos',  to:'medias#photos',  via:'get'
+  # match '/media/photos',  to:'medias#photos',  via:'get'
+  match '/media/photos', to:'static_pages#under_construction', via:'get' 
 
   # Videos (categorized by event+date)
-  match '/media/videos',  to:'medias#videos',  via:'get'
+  # match '/media/videos',  to:'medias#videos',  via:'get'
+  match '/media/videos', to:'static_pages#under_construction', via:'get' 
 
 # Contact 
 # (have a drop down menu with options of who to send to general, localpm, software, etc)
 match '/contact',  to:'contacts#new',  via:'get'
+# match '/contact', to:'static_pages#under_construction', via:'get' 
   resources "contacts", only: [:new, :create]
 
 # Other URLS and links
 
   # Applications
-  match '/applications/membership',  to:'members#new',  via:'get' 
+  match '/applications/membership',  to:'members#new',  via:'get'
+  # match '/applications/membership', to:'static_pages#under_construction', via:'get'
+
   match '/applications/intl_project_team',  to:'members#new_international',  via:'get' 
-  match '/applications/local_project_team',  to:'members#new_local',  via:'get' 
-  match '/applications/software_team',  to:'members#new_software',  via:'get' 
-  match '/applications/executive_board',  to:'members#new_exec_board',  via:'get' 
+  # match '/applications/intl_project_team', to:'static_pages#under_construction', via:'get' 
+
+  # match '/applications/local_project_team',  to:'members#new_local',  via:'get' 
+  match '/applications/local_project_team', to:'static_pages#under_construction', via:'get' 
+
+  # match '/applications/software_team',  to:'members#new_software',  via:'get' 
+  match '/applications/software_team', to:'static_pages#under_construction', via:'get' 
+
+  # match '/applications/executive_board',  to:'members#new_exec_board',  via:'get' 
+  match '/applications/executive_board', to:'static_pages#under_construction', via:'get' 
 
   # Forms
-  match '/forms/project_proposal',  to:'projects#new',  via:'get'
-  match '/forms/event_proposal',  to:'events#new',  via:'get'
+  # match '/forms/project_proposal',  to:'projects#new',  via:'get'
+  match '/forms/project_proposal', to:'static_pages#under_construction', via:'get' 
+
+  # match '/forms/event_proposal',  to:'events#new',  via:'get'
+  match '/forms/event_proposal', to:'static_pages#under_construction', via:'get' 
+
   # match '/forms/order',  to:'X#new',  via:'get'
-
-
-  # User Profiles (gets profile of currently logged in user)
-  # get 'profile', to: 'users#show' 
-
+  match '/forms/order', to:'static_pages#under_construction', via:'get' 
 
 
 #=======================================================================================#
