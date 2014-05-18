437979d6816d59857aebfaef2555b5d8493a5e5e
Josh Kaplan <josh@joshkaplan.org>
2014-05-17 22:31:19 -0400

    helpers files and static_pages

--- main/app/helpers/static_pages_helper.rb
+++ main/app/helpers/static_pages_helper.rb
@@ -1,2 +1,28 @@
 module StaticPagesHelper
+
+    #
+    # Creates an unordered list <ul> with a series of list items <li>
+    # by calling the li function. This function takes a ruby hash containing
+    # text/url pairs to use for the list items.
+    #
+    def ul (list)
+        str = ''
+
+        list.each_key do |i|
+            str += li i, list[i]
+        end
+
+        return '<ul>' + str + '</ul>'
+
+    end
+
+
+    #
+    # Creates a list item <li> and containing anchor tag <a href>.
+    #
+    def li (text, url)
+        return '<li><a href=\'' + url + '\'>' + text + '</a></li>'
+    end
+
+
 end
